7055867e605112df5e067907d78152e6d086f1c7
effieguenther <effiegguenther@gmail.com>
2023-07-17 15:20:06 +0545

    header formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import { Navbar, 
-    NavbarBrand,
+    Dropdown,
+    DropdownItem,
+    DropdownToggle,
+    DropdownMenu,
     Collapse,
     NavbarToggler,
     Nav,
@@ -10,48 +13,56 @@ import { useState } from 'react';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const toggle = () => setDropdownOpen(!dropdownOpen);
 
     return (
-        <Navbar dark color='primary' sticky='top' expand='md'>
-            <NavbarBrand href='/' className='ms-5'>
-                <h1 className='mt-1'>Word of Mouth</h1>
-            </NavbarBrand>
-            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
-            <Collapse isOpen={menuOpen} navbar>
-                <Nav className='ms-auto' navbar>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/'>
-                            Salon
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/'>
-                            Artisan
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/'>
-                            Home Repair
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/'>
-                            Cleaning/Cooking
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/'>
-                            Driving
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/'>
-                            Child/Pet Care
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-            </Collapse>
-        </Navbar>
+        <>
+            <h1 className='mt-3'>Word of Mouth</h1>
+            <Navbar sticky='top' expand='md' >
+                <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+                <Collapse isOpen={menuOpen} navbar>
+                    <Nav className='ms-auto' navbar tabs>
+                        <Dropdown nav isOpen={dropdownOpen} onMouseOver={() => setDropdownOpen(!dropdownOpen)} onMouseOut={() => setDropdownOpen(!dropdownOpen)}>
+                            <DropdownToggle nav caret>
+                                Salon
+                            </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem>massage</DropdownItem>
+                                <DropdownItem>hair</DropdownItem>
+                                <DropdownItem>nails</DropdownItem>
+                                <DropdownItem>wax & thread</DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/'>
+                                Artisan
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/'>
+                                Home Repair
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/'>
+                                Cleaning/Cooking
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/'>
+                                Driving
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/'>
+                                Child/Pet Care
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </>
     );
 };
 
