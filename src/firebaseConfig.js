451bb4cdab20eb3b3e7a619aebb4b68076a6c27c
effieguenther <effiegguenther@gmail.com>
2023-08-01 11:45:17 +0545

    initializing firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwuYMgIUo0xuEX0AyEhgT9CmLW10u7KXk",
+  authDomain: "wordofmouth-alpha.firebaseapp.com",
+  projectId: "wordofmouth-alpha",
+  storageBucket: "wordofmouth-alpha.appspot.com",
+  messagingSenderId: "367231101557",
+  appId: "1:367231101557:web:21da1addcb7b8bce79663c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
